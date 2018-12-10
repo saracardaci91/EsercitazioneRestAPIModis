e8c5faf9a258437ec5e8aa92c5913ca8e58a1e09
saracardaci91 <sara.cardaci91@gmail.com>
2018-12-10 16:50:31 +0100

    aggiunta classe corso

---
+++ ModisAPI/Models/Corso.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModisAPI.Models
+{
+    public class Corso
+    {
+        public int CorsoId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInizio { get; set; }
+        public int DurataInOre { get; set; }
+        public int Livello { get; set; }
+        public int NumeroMassimoPartecipanti { get; set; }
+
+    }
+}
