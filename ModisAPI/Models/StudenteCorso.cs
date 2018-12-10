253f3d864e9bb6961de4832f89fc8a8d09f3abd0
saracardaci91 <sara.cardaci91@gmail.com>
2018-12-10 17:27:00 +0100

    aggiunto relazione studente corso

---
+++ ModisAPI/Models/StudenteCorso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModisAPI.Models
+{
+    public class StudenteCorso
+    {
+        public int StudenteId { get; set; }
+        public Studente Studente { get; set; }
+        public int CorsoId { get; set; }
+        public Corso Corso { get; set; }
+    }
+}
